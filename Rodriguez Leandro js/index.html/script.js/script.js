da6a87a09014a940dec727d73394ec6b7911f8cc
Leancitx <107488682+Leancitx@users.noreply.github.com>
2022-07-26 00:41:39 -0300

    Add files via upload

---
+++ Rodriguez Leandro js/index.html/script.js/script.js
@@ -0,0 +1,14 @@
+let numero1
+ do{
+  numero1 = prompt("¿usted es mayor de 18 años?")
+ numero2 = prompt("ingrese su edad")
+ }
+ while( numero2){
+     numero1 = prompt("¿usted es mayor de 18 años?").toLocaleLowerCase()
+    if(confirmacion == "si"){
+        console.log(numero2)
+    }
+
+}
+ 
+
